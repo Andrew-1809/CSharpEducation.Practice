b3cc213af10fc12e5d0b4ab8225e75097ee880ea
Andrew-1809 <fedandrey93@yandex.ru>
2025-05-12 20:38:57 +0400

    Практическое занятие 2, задания 6 и 7
    Выполнил задание 7 и частично задание 6 из практического занятия 2

---
+++ CSharpEducation.Practice/Practice2.Task6 c/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Practice2.Task6_c
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+            Console.Write("Введите любую фразу: ");                           //6c
+
+            string s = Console.ReadLine();
+            char[] string1 = s.ToCharArray();
+
+            Console.Write("Введите необходимый символ: ");
+            char z = Convert.ToChar(Console.ReadLine());
+
+            int count1 = 0;
+            int count2 = string1.Length;
+
+            for (int i = 0; i < string1.Length; i++)
+            {
+                if (z == string1[i])
+                {
+                    count1++;
+                }
+
+            }
+
+            Console.WriteLine("Число символов " + "'" + z + "'" + " в строке - " + count1 * 100 / count2 + "%");
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Practice2.Task6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Practice2.Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "А роза упала на лапу Азора";                         //6a
+            char[] string1 = s.ToCharArray();
+                       
+            char z = 'а';
+
+            int count1 = 0;
+            int count2 = string1.Length;
+
+            for (int i = 0; i < string1.Length; i++)
+            {                
+                if (z == string1[i])
+                {
+                    count1++;
+                }
+
+            }
+                        
+            Console.WriteLine("Число символов " + "'" + z + "'" + " в строке - " + count1 * 100 / count2 + "%");
+
+        Console.WriteLine(" ");           //Просто разрыв строки для удобства чтения результата в консоли
+
+            Console.Write("Введите любую фразу: ");                           //6b
+                       
+            string ss = Console.ReadLine();
+            char[] string2 = ss.ToCharArray();
+
+            Console.Write("Введите необходимый символ: ");
+            char zz = Convert.ToChar(Console.ReadLine());
+
+            int count11 = 0;
+            int count22 = string1.Length;
+
+            for (int i = 0; i < string2.Length; i++)
+            {
+                if (zz == string2[i])
+                {
+                    count11++;
+                }
+
+            }
+
+            Console.WriteLine("Число символов " + "'" + zz + "'" + " в строке - " + count11 * 100 / count22 + "%");
+                                           
+        }
+    }
+}
