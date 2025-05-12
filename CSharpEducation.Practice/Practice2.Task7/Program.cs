b6f5a9274c6af42e45d7c659a6af7ec7bec3f5dd
Andrew-1809 <fedandrey93@yandex.ru>
2025-05-12 21:00:36 +0400

    Ох ёооо
    Восстановил все случайно удаленные проекты и иже с ними после жесткого косяка

---
+++ CSharpEducation.Practice/Practice2.Task7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Practice2.Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Напиши что-нибудь: ");                            //7a
+            string a = Console.ReadLine();
+            string b = a.ToUpper();
+
+            Console.WriteLine(b);                     
+
+        Console.WriteLine();                     //Просто разрыв строки для удобства чтения результата в консоли
+
+            Console.Write("Напиши что-нибудь еще: ");                         //7b
+            string c = Console.ReadLine();
+            string d = c.ToLower();
+
+            Console.WriteLine(d);
+
+        Console.WriteLine();                     //Просто разрыв строки для удобства чтения результата в консоли
+
+            Console.Write("Напиши что-нибудь еще, ну пожалуйста...: ");                           //7c
+
+            string s = Console.ReadLine();
+            char[] string1 = s.ToCharArray();
+
+           
+            for (int i = 0; i < string1.Length; i++)
+            {
+                if (i == 0)
+                {
+                    string z = Convert.ToString(string1[i]);
+                    Console.Write(z.ToUpper());
+                }
+                else
+                {
+                    string z = Convert.ToString(string1[i]);
+                    Console.Write(z);
+                }
+            }
+                        
+        }
+    }
+}
