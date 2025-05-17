cbb268860eb15e1106908365fe3bf5d20cf17618
Andrew-1809 <fedandrey93@yandex.ru>
2025-05-17 15:40:51 +0400

    Практическое занятие 2, задания 8-16
    Выполнены задания 8-16, загружены все скопом, ибо я лентяй!

---
+++ CSharpEducation.Practice/Practice2.Task10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practice2.Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int z = Convert.ToInt32(args[0]);
+
+            int[] numbers = new int[z];
+
+            Console.WriteLine("Введите по порядку целые числа.");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = numbers[i];
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Practice2.Task15/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Practice2.Task15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = { 1, -1, 9, 7, -4, 5, 0, 2, 15, -12 };
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine(mas[i]);
+
+            }
+
+            int quan = mas.Length;
+            int mid = mas.Length / 2;
+            int temp;
+
+            for (int i = 0; i < mid; i++)
+            {
+                temp = mas[i];
+                mas[i] = mas[quan - i - 1];
+                mas[quan - i - 1] = temp;
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine(mas[i]);
+
+            }
+        }
+    }
+}
