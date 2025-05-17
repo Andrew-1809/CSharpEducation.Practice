55feb1e63c9b67b1997be150c23ceb9a8fd8a42d
Andrew-1809 <fedandrey93@yandex.ru>
2025-05-17 16:14:39 +0400

    Практика 3, задание 1
    Выполнено задание 1 практики 3.

---
+++ CSharpEducation.Practice/Practice3.Task1/Студент.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3.Task1
+{
+    class Студент
+    {
+        string name = "Andrew";
+        int age = 31;
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        }
+    }
+}
