c7520bb2ba42ce4b76f5f9fe58f68cf90b813319
Andrew-1809 <fedandrey93@yandex.ru>
2025-05-19 19:32:46 +0400

    Практика 3, задания 1 и 2
    Выполнено задание 2 и изменено задание 1 (хотя это и не требовалось)

---
+++ CSharpEducation.Practice/Practice3.Task2/Студент.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Practice3.Task2
+{
+    class Студент
+    {
+        public string name = "Andrew";
+        public int age = 31;
+
+        int ср_балл;
+        string error = "Средний балл должен быть от 0 до 5";
+        public int Средний_балл
+        {            
+            set
+            {
+                if (value > 5 || value < 0)
+                    ср_балл = 000;
+
+                else
+                    ср_балл = value;
+            }
+            get
+            {
+                return ср_балл;
+            }
+        }
+        public void Print()
+        {
+            if (Средний_балл == 000)
+                Console.WriteLine($"Имя: {name}  Возраст: {age} Средний балл должен быть в диапазоне от '0' до '5'!");
+            else
+                Console.WriteLine($"Имя: {name}  Возраст: {age} Средний балл: {Средний_балл}");
+        }
+    }
+}
