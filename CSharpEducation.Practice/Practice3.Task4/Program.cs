b0ea2743b2a568ce2f2fcd498e76e749693296bb
Andrew-1809 <fedandrey93@yandex.ru>
2025-05-20 21:01:14 +0400

    Практика 3, задания 3-5
    Выполнены задания 3-5 практики 3.

---
+++ CSharpEducation.Practice/Practice3.Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practice3.Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Книга book_1 = new Книга
+            {
+                Автор = "Пушкин",
+                Название = "Капитанская Дочка"
+            };
+            book_1.Print();
+
+            Книга book_2 = new Книга();       
+            book_2.Print();
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Practice3.Task5/Книга.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3.Task5
+{
+    internal class Книга
+    {
+        public string Название;
+        public string Автор;
+        public int Год_издания;
+               
+        public Книга() : this("Неизвестно") { }
+        public Книга(string name) : this(name, "Пушкин", 101) { }
+        public Книга(string name, string author, int year)
+        {
+            Название = name;
+            Автор = author;
+            Год_издания = year;
+        }
+              
+
+        public void Print()
+        {
+            Console.WriteLine($"Книга '{Название}' автора {Автор} {Год_издания} года издания.");
+        }
+
+    }
+}
