ad1f9f963503ea35028a5d1da61ea57b32b944d5
Andrew-1809 <fedandrey93@yandex.ru>
2025-05-25 10:47:19 +0400

    Практика 4
    Задания 1-3 практики 4 выполнены

---
+++ CSharpEducation.Practice/ClassLibrary_Person/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary_Person
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+        public void Print()
+        {
+            Console.WriteLine($"Человек по имени {name} возрастом {age}");
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Practice4.Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ClassLibrary_Person;
+
+namespace Practice4.Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Person_1 = new Person() { age = 21, name = "Ivan"};
+            Person_1.Print();
+
+            Person Person_2 = new Person();
+            Person_2.Print();
+
+            Person Person_3 = new Person() { age = 98, name = "Anatoly"};
+            Person_3.Print();
+        }
+    }
+}
